4070fea9d03211a2454914aa4a5e21c1f7d7401b
Jean-Leherle <j.leherle@gmail.com>
2022-06-22 13:24:54 +0200

    Etape5.1 Session initialsé

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // Toujours commencer par importer les variables d'environnement !
 require('dotenv').config();
+const session = require('express-session');
 
 const express = require('express');
 
@@ -11,12 +12,23 @@ const PORT = process.env.PORT || 5000;
 
 
 const app = express();
+//parametrage de la session
+app.use(session({
+  resave: true,
+  saveUninitialized: true,
+  secret: "fhsdjfghaqeryuifghuirdqhguirehguiserhifgsdhjih",
+  cookie: {
+    secure: false,
+    maxAge: (1000*60*60) // ça fait une heure
+  }
+}));
 
 app.set("view engine", "ejs");
 app.set("views", "app/views");
 app.use(express.static("app/public"));
 
 // servir les fichiers statiques qui sont dans "integration"
+
 
 // routage !
 app.use(router);
